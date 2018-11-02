93e2f0950f5f40097b560c6d99d602d03ca214e0
ihor <ihor.pismennyy@gmail.com>
2018-11-02 15:29:01 +0200

    with quotes

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import os
 import redis
+import random
 
 app = Flask(__name__)
 
 r = None
+
+
 @app.route('/')
 def hello():
-    return 'Hello, fellow traveler!. Follow white <a href="/rabbit">rabbit</a>!'
+    return 'Hello, fellow traveler! Follow white <a href="/rabbit">rabbit</a>!'
 
 
 @app.route('/<name>')
@@ -17,13 +20,14 @@ def hello_name(name):
         return 'Be smart, {}: <br/>{}'.format(name, quote)
     else:
         put_in_mind(name, get_random_quote())
-        return 'Hello, {}! We will remember you on your next visit!' \
-               'Refresh page or follow this <a href="/{}"></a>to see what happens'.format(
+        return 'Hello, {}! We will remember you on your next visit!<br/>' \
+               'Refresh page or follow this <a href="/{}">link</a> to see what happens'.format(
             name, name)
 
 
 if __name__ == '__main__':
     app.run()
+
 
 def connect_redis():
     global r
@@ -31,7 +35,7 @@ def connect_redis():
 
 
 def get_random_quote():
-    return 'ha-ha-ha'
+    return random.choice(quotes)
 
 
 def put_in_mind(name, quote):
@@ -42,3 +46,16 @@ def put_in_mind(name, quote):
 def get_from_mind(name):
     if r is None: connect_redis()
     return r.get(name)
+
+
+quotes = ["'And what is the use of a book,' thought Alice, 'without pictures or conversation?'",
+          "It would have made a dreadfully ugly child; but it makes rather a handsome pig.",
+          "'Who in the world am I?' Ah, that's the great puzzle!",
+          "It takes all the running you can do, to keep in the same place. If you want to get somewhere else, you must run at least twice as fast as that!",
+          "I don't think -- " "Then you shouldn't talk.",
+          "Curiouser and curiouser!",
+          "We're all mad here.",
+          "It's no use going back to yesterday, because I was a different person then.",
+          "Why, sometimes I've believed as many as six impossible things before breakfast.",
+          "Off with their heads!"
+          ]
